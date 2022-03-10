48f83a3bdc67ed82a38c12c8b8221e20c19f163b
hys <2811631650@qq.com>
2022-03-10 16:09:43 +0800

    finish palindrome.c and Makefile

--- src/palindrome.c
+++ src/palindrome.c
@@ -4,6 +4,11 @@ int main()
 	int n;
 	scanf("%d",&n);
 
+	int tmp = n, ans = 0;
+	while(tmp > 0){
+		ans = tmp % 10 + ans * 10;
+		tmp /= 10;
+	}
 
 
 
@@ -13,8 +18,7 @@ int main()
 
 
 
-
-	if(){
+	if(ans == n){
 		printf("Y");
 	}else{
 		printf("N");
