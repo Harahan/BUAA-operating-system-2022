211190003ce4684a9e28701fbf6aa566c4ffed15
hys <2811631650@qq.com>
2022-03-17 21:23:08 +0800

    delete trash, again

--- ray/sh_test2/search.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-#First you can use grep (-n) to find the number of lines of string.
-#Then you can use awk to separate the answer.
-grep -n $2 $1 > test 
-awk -F: '{print $1}' test > $3
-rm test
