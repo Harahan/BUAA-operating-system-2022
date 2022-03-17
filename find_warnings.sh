d6fb2d41f51ecc5b8a07eb9d87d59a95f5ca379e
hys <2811631650@qq.com>
2022-03-17 15:51:06 +0800

    xx

--- find_warnings.sh
+++ find_warnings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcc -Wall $1 -o test 2> warning.txt
-grep warning:  warning.txt > result.txt
+grep warning warning.txt > result.txt
 sed  -i 's/warning: //g' result.txt
 gcc $1 -o test
 if [ $? -eq 0 ]
