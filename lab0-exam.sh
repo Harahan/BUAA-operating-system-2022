748788aaf425eb20f4aecb323163590850e3a641
hys <2811631650@qq.com>
2022-03-17 14:24:40 +0800

    v_4

--- lab0-exam.sh
+++ lab0-exam.sh
@@ -5,5 +5,5 @@ mkdir hello_os_dir
 cp os_hello ./hello_os_dir
 cp os_hello ./hello_os_dir/hello_os
 rm os_hello
-grep -r -n os_hello ./hello_os.c > test
+grep -r -n os_hello ./hello_os.c > hello_os.txt
 
