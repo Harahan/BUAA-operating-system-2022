e9be9510d89790bae6871aa61b2f6b7e7d600457
Huang YuShi <20376156@buaa.edu.cn>
2022-03-23 01:57:24 +0800

    finish1.5-v2 (negFlag correct)

--- lib/print.c
+++ lib/print.c
@@ -145,6 +145,10 @@ lp_Print(void (*output)(void *, char *, int),
 			Think the difference between case 'd' and others. (hint: negFlag).
 		*/
         /*************************************************************************************************/
+        if (num < 0) {
+            num = -num;
+            negFlag = 1;
+        }
         length = PrintNum(buf, num, 10, negFlag, width, ladjust, padc, 0);
         OUTPUT(arg, buf, length);
         /*************************************************************************************************/
