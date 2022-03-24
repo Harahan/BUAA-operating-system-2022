7dab5173206d6f6192a721febb896a59c2001df6
hys <2811631650@qq.com>
2022-03-24 15:40:32 +0800

    v6

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -91,7 +91,7 @@ int readelf(u_char *binary, int size)
 			phdr2 = (Elf32_Phdr*)(ptr_ph_table + (Nr + 1) * ph_entry_size);
 			p_vaddr1 = phdr1->p_vaddr; p_vaddr2 = phdr2->p_vaddr;
 			p_filesz1 = phdr1->p_filesz; p_filesz2 = phdr2->p_filesz;
-			p1 = p_vaddr1 / S + 1; p2 = p_vaddr2 / S + 1; 
+			p1 = p_vaddr1 / S; p2 = p_vaddr2 / S; 
 			q1 = (p_vaddr1 + p_filesz1) / S; q2 = (p_vaddr2 + p_filesz2) / S;
 			if (q1 < p2) {
    				printf("%d:0x%x,0x%x\n", Nr, phdr1->p_filesz, phdr1->p_memsz);
