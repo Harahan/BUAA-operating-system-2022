ae27c8c7ab9faa55a3f81090de4d79fb3df8bce4
hys <2811631650@qq.com>
2022-03-24 15:52:29 +0800

    v11

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -95,13 +95,13 @@ int readelf(u_char *binary, int size)
 			q1 = (p_vaddr1 + p_filesz1) / S; q2 = (p_vaddr2 + p_filesz2) / S;
 			if (q1 <= p2) {
    				printf("%d:0x%x,0x%x\n", Nr, phdr1->p_filesz, phdr1->p_memsz);
-			} else if (p_vaddr1 + p_filesz1 <= p_vaddr2 && q1 >= p2) {
+			} else if (p_vaddr1 + p_filesz1 <= p_vaddr2) {
 				printf("Overlay at page va : 0x%x\n", p_vaddr2);
 			} else {
 				printf("Conflict at page va : 0x%x\n", p_vaddr2);
 			}
 		}
-   		printf("%d:0x%x,0x%x\n", Nr, phdr1->p_filesz, phdr1->p_memsz);
+   		printf("%d:0x%x,0x%x\n", Nr, phdr2->p_filesz, phdr2->p_memsz);
 		
 		
 
