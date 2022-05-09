0f2a7569ec615275dfaeb62d8f1d6788aef089fc
Huang YuShi <20376156@buaa.edu.cn>
2022-05-09 19:42:51 +0800

    finish lab4_3

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -286,8 +286,8 @@ int sys_set_env_status(int sysno, u_int envid, u_int status)
     if ((ret = envid2env(envid, &env, 0)) < 0) return ret;
     if (status == ENV_RUNNABLE && env->env_status != ENV_RUNNABLE)
         LIST_INSERT_HEAD(&env_sched_list[0], env, env_sched_link);
-    //else if (status == ENV_NOT_RUNNABLE && env->env_status == ENV_RUNNABLE)
-        //LIST_REMOVE(env, env_sched_link); // ?
+    else if (status == ENV_NOT_RUNNABLE && env->env_status == ENV_RUNNABLE)
+        LIST_REMOVE(env, env_sched_link); // ?
 
     env->env_status = status;
 
